86e6bf46b4c7c26798c5efd506777760ce0f4946
Fatima Ayala <ftayala89@gmail.com>
2021-08-12 08:05:24 -0600

    Cambios nuevos y nueva practica agregada

---
+++ ClaseTeorica1208/Ejercicio1Clas1208.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseTeorica1208
+{
+    class Ejercicio1Clas1208
+    {
+    }
+}
